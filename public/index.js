df50c909a2b297f9ddc28e1ddd34ee51a608240b
mdecat <maarten.de.cat@sarens.com>
2022-12-15 19:52:03 +0100

    commit question 3

--- public/index.js
+++ public/index.js
@@ -1,4 +1,31 @@
-const onResponse = (response) => {};
+const movies = [];
+const movieList = document.getElementById("list");
+const onResponse = (response) => {
+  response?.forEach( (movie) => {
+    addMovieToDOM(movie);
+   });
+};
+
+const addMovieToDOM = (movie) => {
+  const movieTile = document.createElement("a");
+  const movieBox = document.createElement("div");
+  const movieImage = document.createElement("img");
+  const movieBoxTitle = document.createElement("div");
+  const movieTitle = document.createElement("p");
+
+  
+  movieBox.className = "movieBox";
+  movieTitle.innerText = movie?.Title;
+  movieImage.src = movie?.Poster;
+  movieTile.href = `/movie?movieId=${movie.Id}`;
+
+  movieTile.appendChild(movieBox);
+  movieBox.appendChild(movieImage);
+  movieBox.appendChild(movieBoxTitle);
+  movieBoxTitle.appendChild(movieTitle);
+
+  movieList.appendChild(movieTile);
+};
 
 fetch("https://europe-west1-javascript-lessons-tijl.cloudfunctions.net/movies")
   .then((res) => res.json())
--- public/movie.js
+++ public/movie.js
@@ -1,4 +1,33 @@
-const onResponse = (response) => {};
+const url = window.location.href;
+const movieId = url.split('movieId=')[1];
+
+const movieDetailPoster = document.getElementById('image');
+const movieDetailTitle = document.getElementById('movieTitle');
+const movieDetailMovieRuntime = document.getElementById('movieRuntime');
+const movieDetailMovieRoom = document.getElementById('movieRoom');
+const movieDetailMoviePlot = document.getElementById('moviePlot');
+const movieDetailMovieWriter = document.getElementById('movieWriter');
+const movieDetailMovieDirector = document.getElementById('movieDirector');
+const movieDetailMovieActors = document.getElementById('movieActors');
+
+//genreList
+
+const onResponse = (response) => {
+  const selectedMovie = response.find((m) => m.Id === movieId);
+  addMovieDetailsToDOM(selectedMovie);
+};
+
+const addMovieDetailsToDOM = (movieDetails) =>{
+  movieDetailPoster.src = movieDetails.Poster;
+  movieDetailTitle.innerText = movieDetails.Title;
+  movieDetailMovieRuntime.innerText = movieDetails.Runtime;
+  movieDetailMovieRoom.innerText = `Room: ${movieDetails.Room}`;
+  movieDetailMoviePlot.innerText = movieDetails.Plot;
+  movieDetailMovieWriter.innerText = movieDetails.Writer;
+  movieDetailMovieDirector.innerText = movieDetails.Director;
+  movieDetailMovieActors.innerText = movieDetails.Actors.join(' - ');
+
+}
 
 fetch("https://europe-west1-javascript-lessons-tijl.cloudfunctions.net/movies")
   .then((res) => res.json())
