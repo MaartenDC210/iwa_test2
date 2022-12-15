d1bfe1bccad5d47a3b7e447462b93b0e1fdb05ad
mdecat <maarten.de.cat@sarens.com>
2022-12-15 18:34:00 +0100

    First Commit

---
+++ public/movie.js
@@ -0,0 +1,5 @@
+const onResponse = (response) => {};
+
+fetch("https://europe-west1-javascript-lessons-tijl.cloudfunctions.net/movies")
+  .then((res) => res.json())
+  .then((res) => onResponse(res));
